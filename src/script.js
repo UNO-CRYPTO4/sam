47745e52db1e53aa598ee969d659c7843dd9fc1c
UNO_CRYTO4 <judithagudey1@gmail.com>
2026-09-14 22:49:59 +0000

    added changes & styling & images

---
+++ src/script.js
@@ -0,0 +1,22 @@
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+    (entries) => {
+
+        entries.forEach((entry) => {
+
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+            }
+
+        });
+
+    },
+    {
+        threshold: 0.12
+    }
+);
+
+reveals.forEach((section) => {
+    observer.observe(section);
+});
